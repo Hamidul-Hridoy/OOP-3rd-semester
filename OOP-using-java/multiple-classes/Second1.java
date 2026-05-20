0d0b0a3348fff2a2e5e08e07368a24e9bc6b3f12
Hamidul Hridoy <hamidulhridoy05@gmail.com>
2026-05-20 10:34:19 +0600

    fifth

---
+++ OOP-using-java/multiple-classes/Second1.java
@@ -0,0 +1,7 @@
+class Second1 {
+  public static void main(String[] args) {
+    Main1 myCar = new Main1();     // Create a myCar object
+    myCar.fullThrottle();      // Call the fullThrottle() method
+    myCar.speed(200);          // Call the speed() method
+  }
+}
